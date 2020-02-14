c075beab65a980b61dfbb71121e0347b2171bc60
Mow69 <mouazmouaz@hotmail.fr>
2020-02-14 14:48:16 +0100

    add medias.html, media-config.html, .animate() jquery function in main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(function() {
+  // function pour rendre visible ou cache le password au clic :
   $(".iconInput i").click(function() {
     $(this).toggleClass("fa-eye-slash");
     $(this).toggleClass("fa-eye");
@@ -11,6 +12,23 @@ $(function() {
         .prev()
         .attr("type", "password");
     }
+  });
+
+  // Anime la progression (via l'augmentation du width) de la barre de chargement
+  $("#loading").on("shown.bs.modal", function(e) {
+    $("#loading .progress-bar").animate(
+      { width: "100%" },
+      3000,
+      // Redirige vers medias-config au bout de 3sec
+      function() {
+        location.href = "media-config.html";
+      }
+    );
+  });
+
+  // Permet le retour à 0 de la valeur du width quand la modal est hidden
+  $("#loading").on("hidden.bs.modal", function(e) {
+    $("#loading .progress-bar").css("width", "0%");
   });
 });
 
